6e54f0a9bda638c5573ace65ce966a01d041173f
gos337 <gos337@naver.com>
2024-03-12 15:29:00 +0900

    -11, 13

--- a11_pickids/android/app/src/main/kotlin/com/example/a11_pickids/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.a11_pickids
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- a11_pickids/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:a11_pickids/5_view/PicKidsApp.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const PicKidsApp());
-}
